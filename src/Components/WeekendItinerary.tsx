d2a8eda6383a3adfe1e0a449fd783f4d0e929be8
syneva-providence <syneva.runyan@providence.org>
2025-05-14 10:51:02 -0800

    update faqs and remove fireworks

--- src/Components/WeekendItinerary.tsx
+++ src/Components/WeekendItinerary.tsx
@@ -9,7 +9,6 @@ function WeekendItinerary() {
                 <li><strong>4pm - 6pm</strong> Arrival, welcome drinks, and yard games</li>
                 <li><strong>7pm - 8ish</strong> Welcome dinner</li>
                 <li><strong>8ish - 10pm</strong> Socializing & s'mores</li>
-                <li><strong>10pm</strong> Fireworks</li>
            </ul>
             <h3 >Friday, June 6th</h3>
             <ul>
@@ -18,7 +17,7 @@ function WeekendItinerary() {
                 <li><strong>12:30pm - 2pm</strong> Lunch</li>
                 <li><strong>2:30pm - 5pm</strong> (Optional) Non-alcoholic and alcoholic group tastings followed by Singo</li>
                 <li><strong>5pm - 6pm</strong> (Wedding Party Only*) Wedding Rehearsal</li>
-                <li><strong>7pm - 8pm</strong>  Rehearsal Dinner</li>
+                <li><strong>7pm - 8pm</strong>  Rehearsal Dinner (Dress code - <a href="https://www.theknot.com/content/what-to-wear-casual" target="_blank" rel="noopener noreferrer" >smart casual</a>)</li>
                 <p><em>*all other events are open to all guests</em></p>
             </ul>
             <h3 >Saturday, June 7th</h3>
@@ -26,7 +25,7 @@ function WeekendItinerary() {
                 <li><strong>8:30am</strong> Continental Breakfast</li>
                 <li><strong>10:30am</strong> Wedding Day brunch</li>
                 <li><strong>Noon - 3pm</strong> Optional hiking, yard games, socializing</li>
-                <li><strong>4pm - 4:30pm</strong> Wedding Ceremony</li>
+                <li><strong>4pm - 4:30pm</strong> Wedding Ceremony (Dress code - semi-formal)</li>
                 <li><strong>4:30 - 6pm</strong> Cocktail Party</li>
                 <li><strong>6pm - late</strong> Dinner + Reception</li>
             </ul>
